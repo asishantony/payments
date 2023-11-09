136a142eee1492392d6456dae55e6df892086559
Asish K Antony <asishantony@gmail.com>
2023-11-09 15:34:44 +0530

    Added the form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
-let theme = document.getElementById('theme');
+let theme = document.getElementById("theme");
 console.log(theme);
-theme.addEventListener('change', function () {
-    if (this.checked) {
-        document.body.classList.add('light-theme');
-    } else {
-        document.body.classList.remove('light-theme');
-    }
-})
+theme.addEventListener("change", function () {
+  if (this.checked) {
+    document.body.classList.add("light-theme");
+  } else {
+    document.body.classList.remove("light-theme");
+  }
+});
+
